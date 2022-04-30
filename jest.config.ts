8131dc2d2cd03e9c9c9b161aeac07a1e1c80063d
Albert <albert.rocha@r2t.company>
2022-04-30 20:14:27 -0300

    chore: project configuration: lint, jest and typescript

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+
+import { compilerOptions } from "./tsconfig.json";
+
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts"],
+};
