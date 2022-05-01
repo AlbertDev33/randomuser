e310d91f1666cf3133950dabab65ee9b55dcf6a9
Albert <albert.rocha@r2t.company>
2022-04-30 23:35:32 -0300

    improvement: added results property

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,43 +1,47 @@
 export type User = {
-  gender: string;
-  name: {
-    first: string;
-    last: string;
-  };
-  location: {
-    street: string;
-    city: string;
-    state: string;
-    postcode: string;
-    coordinates: {
-      latitude: string;
-      longitude: string;
-    };
-  };
-  email: string;
-  login: {
-    uuid: string;
-    username: string;
-    password: string;
-  };
-  dob: {
-    date: Date;
-    age: number;
-  };
-  registered: {
-    date: Date;
-    age: number;
-  };
-  phone: string;
-  cell: string;
-  id: {
-    name: string;
-    value: string;
-  };
-  picture: {
-    large: string;
-    medium: string;
-    thumbnail: string;
-  };
-  nat: string;
+  results: [
+    {
+      gender: string;
+      name: {
+        first: string;
+        last: string;
+      };
+      location: {
+        street: string;
+        city: string;
+        state: string;
+        postcode: string;
+        coordinates: {
+          latitude: string;
+          longitude: string;
+        };
+      };
+      email: string;
+      login: {
+        uuid: string;
+        username: string;
+        password: string;
+      };
+      dob: {
+        date: Date;
+        age: number;
+      };
+      registered: {
+        date: Date;
+        age: number;
+      };
+      phone: string;
+      cell: string;
+      id: {
+        name: string;
+        value: string;
+      };
+      picture: {
+        large: string;
+        medium: string;
+        thumbnail: string;
+      };
+      nat: string;
+    },
+  ];
 };
