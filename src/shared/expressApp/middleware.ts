dca325313c3e63deb46eaf8a8c15a5e42591c7db
Albert <albert.rocha@r2t.company>
2022-04-30 23:38:59 -0300

    improvement: user router in app

--- src/shared/expressApp/middleware.ts
+++ src/shared/expressApp/middleware.ts
@@ -1,17 +1,18 @@
 import cors from 'cors';
-import express, { Express, NextFunction, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 import 'express-async-errors';
+
+import { router } from '../routes';
 
 export default (app: Express): void => {
   app.use(cors({ origin: '*' }));
   app.use(express.json());
+  app.use(router);
 
-  app.use(
-    async (err: IError, _: Request, response: Response, next: NextFunction) => {
-      return response.status(err.statusCode).json({
-        statusCode: err.statusCode,
-        message: err.message,
-      });
-    },
-  );
+  app.use(async (err: IError, _: Request, response: Response) => {
+    return response.status(err.statusCode).json({
+      statusCode: err.statusCode,
+      message: err.message,
+    });
+  });
 };
