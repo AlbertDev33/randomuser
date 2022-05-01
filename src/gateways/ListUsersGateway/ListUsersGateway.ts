5169fb9afd1e4e9babd053d16a11e35be936add3
Albert <albert.rocha@r2t.company>
2022-04-30 23:37:31 -0300

    feature: API call implementation

--- src/gateways/ListUsersGateway/ListUsersGateway.ts
+++ src/gateways/ListUsersGateway/ListUsersGateway.ts
@@ -1,9 +1,16 @@
 import { User } from '@entities/User';
-
-import { IListUsersGateway } from './protocol/IListUsersGateway';
+import { IListUsersGateway } from '@shared/interfaces/IListUsersGateway';
+import { IHttpRequests } from '@shared/interfaces/IRequests';
 
 export class ListUsersGateway implements IListUsersGateway {
-  public async list(): Promise<User[]> {
-    throw new Error('Method not implemented.');
+  constructor(private request: IHttpRequests) {}
+
+  public async list(): Promise<User> {
+    try {
+      const { data } = await this.request.get<User>(process.env.API_URL);
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
 }
