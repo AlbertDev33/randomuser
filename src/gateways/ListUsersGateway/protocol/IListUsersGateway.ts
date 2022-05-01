9e3fdb33dc7ddcadafdfc02d85bb1af99c04a806
Albert <albert.rocha@r2t.company>
2022-04-30 23:33:15 -0300

    improvement: changed file path

--- src/gateways/ListUsersGateway/protocol/IListUsersGateway.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '@entities/User';
-
-export interface IListUsersGateway {
-  list(): Promise<User[]>;
-}
--- src/services/ListUsers/protocol/IListUsers.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '@entities/User';
-
-export interface IListUsers {
-  execute(): Promise<User[]>;
-}
